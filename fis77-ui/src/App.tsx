ab0ab64c7ca226c6efe96f2c5cbae97b1c63db58
Matej-Omazic <omazzza@gmail.com>
2024-07-24 16:46:19 +0200

    Implemented default unknown route.

--- fis77-ui/src/App.tsx
+++ fis77-ui/src/App.tsx
@@ -7,6 +7,7 @@ import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
+import NotFound from "./pages/NotFound";
 
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 
@@ -73,6 +74,7 @@ function App() {
           path="/contact"
           element={<Contact className={className} langName={langName} />}
         />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       <ScrollToTopButton className={className} />
---
+++ fis77-ui/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+// src/components/NotFound.jsx
+import React from "react";
+
+const NotFound = () => {
+  return <div className="text-center bg-red-900">Not Found</div>;
+};
+
+export default NotFound;
