0cb2f7d55ccbc420f18af5e44d3edb015af7a117
Matej-Omazic <omazzza@gmail.com>
2024-10-02 17:41:00 +0200

    Improved SEO on componenets.

--- fis77-ui/src/components/Footer.tsx
+++ fis77-ui/src/components/Footer.tsx
@@ -11,87 +11,103 @@ interface IProps {
 const Footer: FC<IProps> = ({ langName }) => {
   return (
     <footer className="text-white p-4 bg-zinc-800 font-semibold">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="">
-          <p>
-            {langName === "eng"
-              ? textContent.eng.first_element
-              : textContent.cro.first_element}
-          </p>
-          <p>
-            {langName === "eng"
-              ? textContent.eng.second_element
-              : textContent.cro.second_element}
-          </p>
-          <p>
-            {langName === "eng"
-              ? textContent.eng.third_element
-              : textContent.cro.third_element}
-          </p>
-          <p>
-            {langName === "eng"
-              ? textContent.eng.fourth_element
-              : textContent.cro.fourth_element}
-          </p>
-        </div>
+      <div className="container mx-auto flex flex-col md:flex-row items-start justify-between">
+        <section>
+          <address className="not-italic">
+            <p>
+              {langName === "eng"
+                ? textContent.eng.first_element
+                : textContent.cro.first_element}
+            </p>
+            <p>
+              {langName === "eng"
+                ? textContent.eng.second_element
+                : textContent.cro.second_element}
+            </p>
+            <p>
+              {langName === "eng"
+                ? textContent.eng.third_element
+                : textContent.cro.third_element}
+            </p>
+            <p>
+              {langName === "eng"
+                ? textContent.eng.fourth_element
+                : textContent.cro.fourth_element}
+            </p>
+          </address>
+        </section>
 
-        <div className="mt-4 md:mt-0">
+        <section className="mt-4 md:mt-0">
           <p className="">
             {langName === "eng"
               ? textContent.eng.fifth_element
               : textContent.cro.fifth_element}
           </p>
-          <div className="flex space-x-4 mt-1">
-            <a
-              href="https://www.facebook.com/fotostudioivan77/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Facebook link"
-            >
-              <img
-                className="w-7 hover:scale-110 transition ease-out"
-                src={fbLogo}
-                alt="Facebook icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/foto_studio_ivan_/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Instagram link"
-            >
-              <img
-                className="w-7 hover:scale-110 transition ease-out"
-                src={igLogo}
-                alt="Instagram icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/booth360event/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Second Instagram link"
-            >
-              <img
-                className="w-7 hover:scale-110 transition ease-out"
-                src={igLogo}
-                alt="Second Instagram icon"
-              />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCSHL75T2yClooloeAyzZXiw"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Youtube link"
-            >
-              <img
-                className="w-8 hover:scale-110 transition ease-out"
-                src={ytLogo}
-                alt="Youtube icon"
-              />
-            </a>
-          </div>
-        </div>
+          <nav aria-label="Social Media Links">
+            <ul className="flex space-x-4 mt-1">
+              <li>
+                <a
+                  href="https://www.facebook.com/fotostudioivan77/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Visit our Facebook page"
+                  title="Facebook Page"
+                >
+                  <img
+                    className="w-7 hover:scale-110 transition ease-out"
+                    src={fbLogo}
+                    alt="Facebook logo for Foto Studio Ivan"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/foto_studio_ivan_/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Visit our Instagram profile"
+                  title="Instagram Profile"
+                >
+                  <img
+                    className="w-7 hover:scale-110 transition ease-out"
+                    src={igLogo}
+                    alt="Instagram logo for Foto Studio Ivan"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/booth360event/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Visit Booth 360 Event Instagram profile"
+                  title="Instagram Profile for Booth 360 Event"
+                >
+                  <img
+                    className="w-7 hover:scale-110 transition ease-out"
+                    src={igLogo}
+                    alt="Instagram logo for Booth 360 Event"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCSHL75T2yClooloeAyzZXiw"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Visit our Youtube channel"
+                  title="YouTube Channel"
+                >
+                  <img
+                    className="w-8 hover:scale-110 transition ease-out"
+                    src={ytLogo}
+                    alt="YouTube logo for Foto Studio Ivan"
+                  />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </div>
     </footer>
   );
--- fis77-ui/src/components/Navbar.tsx
+++ fis77-ui/src/components/Navbar.tsx
@@ -43,113 +43,115 @@ const Navbar: FC<IProps> = ({
   };
 
   return (
-    <div className={`theme-${className}-main theme-${className}-text`}>
-      <nav>
+    <header className={`theme-${className}-main theme-${className}-text`}>
+      <nav aria-label="Main Navigation">
         <div className="max-w-7xl mx-auto">
-          <div className="flex mx-auto justify-between w-5/6 ">
+          <div className="flex mx-auto justify-between w-5/6">
             {/* Primary menu and logo */}
             <div className="flex items-center gap-16 my-5">
               {/* logo */}
-              {className === "light" ? (
+              <a href="/" onClick={goToHomePage} aria-label="Home Page">
                 <img
-                  onClick={goToHomePage}
                   className="w-24 rounded-xl"
-                  src={logoImg}
-                  alt=""
+                  src={className === "light" ? logoImg : logoImgInvert}
+                  alt="Company Logo"
                 />
-              ) : (
-                <img
-                  onClick={goToHomePage}
-                  className="w-24 rounded-xl"
-                  src={logoImgInvert}
-                  alt=""
-                />
-              )}
+              </a>
               <div>
                 <a className="flex gap-1 font-bold text-gray-700 items-center "></a>
               </div>
-              {/* primary */}
-              <div className="hidden lg:flex gap-8 ">
-                <Link
-                  to="/"
-                  className={`hover:text-red-500 font-medium relative group ${
-                    location.pathname === "/" ? "border-b-2 border-red-500" : ""
-                  }`}
-                >
-                  {langName === "eng"
-                    ? textContent.eng.first_route
-                    : textContent.cro.first_route}
-                  {location.pathname !== "/" && (
-                    <span className="absolute bottom-0 left-0 w-full h-0.5 bg-red-500 transform origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                  )}
-                </Link>
-                <Link
-                  to="/services"
-                  className={`hover:text-red-500 font-medium relative group ${
-                    location.pathname === "/services"
-                      ? "border-b-2 border-red-500"
-                      : ""
-                  }`}
-                >
-                  {langName === "eng"
-                    ? textContent.eng.second_route
-                    : textContent.cro.second_route}
-                  {location.pathname !== "/services" && (
-                    <span className="absolute bottom-0 left-0 w-full h-0.5 bg-red-500 transform origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                  )}
-                </Link>
-                <Link
-                  to="/gallery"
-                  className={`hover:text-red-500 font-medium relative group ${
-                    location.pathname === "/gallery"
-                      ? "border-b-2 border-red-500"
-                      : ""
-                  }`}
-                >
-                  {langName === "eng"
-                    ? textContent.eng.third_route
-                    : textContent.cro.third_route}
-                  {location.pathname !== "/gallery" && (
-                    <span className="absolute bottom-0 left-0 w-full h-0.5 bg-red-500 transform origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                  )}
-                </Link>
-                <Link
-                  to="/about"
-                  className={`hover:text-red-500 font-medium relative group ${
-                    location.pathname === "/about"
-                      ? "border-b-2 border-red-500"
-                      : ""
-                  }`}
-                >
-                  {langName === "eng"
-                    ? textContent.eng.fourth_route
-                    : textContent.cro.fourth_route}
-                  {location.pathname !== "/about" && (
-                    <span className="absolute bottom-0 left-0 w-full h-0.5 bg-red-500 transform origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                  )}
-                </Link>
-                <Link
-                  to="/contact"
-                  className={`hover:text-red-500 font-medium relative group ${
-                    location.pathname === "/contact"
-                      ? "border-b-2 border-red-500"
-                      : ""
-                  }`}
-                >
-                  {langName === "eng"
-                    ? textContent.eng.fifth_route
-                    : textContent.cro.fifth_route}
-                  {location.pathname !== "/contact" && (
-                    <span className="absolute bottom-0 left-0 w-full h-0.5 bg-red-500 transform origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                  )}
-                </Link>
-              </div>
+              {/* Primary Navigation */}
+              <ul className="hidden lg:flex gap-8" role="menubar">
+                <li role="none">
+                  <Link
+                    to="/"
+                    role="menuitem"
+                    className={`hover:text-red-500 font-medium relative group ${
+                      location.pathname === "/"
+                        ? "border-b-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={handleLinkClick}
+                  >
+                    {langName === "eng"
+                      ? textContent.eng.first_route
+                      : textContent.cro.first_route}
+                  </Link>
+                </li>
+                <li role="none">
+                  <Link
+                    to="/services"
+                    role="menuitem"
+                    className={`hover:text-red-500 font-medium relative group ${
+                      location.pathname === "/services"
+                        ? "border-b-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={handleLinkClick}
+                  >
+                    {langName === "eng"
+                      ? textContent.eng.second_route
+                      : textContent.cro.second_route}
+                  </Link>
+                </li>
+                <li role="none">
+                  <Link
+                    to="/gallery"
+                    role="menuitem"
+                    className={`hover:text-red-500 font-medium relative group ${
+                      location.pathname === "/gallery"
+                        ? "border-b-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={handleLinkClick}
+                  >
+                    {langName === "eng"
+                      ? textContent.eng.third_route
+                      : textContent.cro.third_route}
+                  </Link>
+                </li>
+                <li role="none">
+                  <Link
+                    to="/about"
+                    role="menuitem"
+                    className={`hover:text-red-500 font-medium relative group ${
+                      location.pathname === "/about"
+                        ? "border-b-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={handleLinkClick}
+                  >
+                    {langName === "eng"
+                      ? textContent.eng.fourth_route
+                      : textContent.cro.fourth_route}
+                  </Link>
+                </li>
+                <li role="none">
+                  <Link
+                    to="/contact"
+                    role="menuitem"
+                    className={`hover:text-red-500 font-medium relative group ${
+                      location.pathname === "/contact"
+                        ? "border-b-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={handleLinkClick}
+                  >
+                    {langName === "eng"
+                      ? textContent.eng.fifth_route
+                      : textContent.cro.fifth_route}
+                  </Link>
+                </li>
+              </ul>
             </div>
             {/* Secondary menu */}
             <div className="flex gap-6">
               {/* Mobile navigation toggle */}
               <div className="lg:hidden flex items-center">
-                <button onClick={() => setToggleMenu(!toggleMenu)}>
+                <button
+                  onClick={() => setToggleMenu(!toggleMenu)}
+                  aria-label="Toggle Mobile Menu"
+                >
                   <svg
                     className="w-6 h-6"
                     fill="none"
@@ -166,9 +168,13 @@ const Navbar: FC<IProps> = ({
                   </svg>
                 </button>
               </div>
-              {/* Toggle for Lang Mode */}
+              {/* Language and Theme Switch */}
               <div className="flex items-center">
+                <label htmlFor="language-select" className="sr-only">
+                  Select Language
+                </label>
                 <select
+                  id="language-select"
                   value={isLangEng ? "ENG" : "CRO"}
                   onChange={(e) => {
                     const newLang = e.target.value === "ENG";
@@ -178,31 +184,30 @@ const Navbar: FC<IProps> = ({
                   }}
                   className={`theme-${className}-main theme-${className}-text relative inline-flex items-center cursor-pointer rounded-lg`}
                 >
-                  <option className={`text-center`} value="CRO">
-                    CRO
-                  </option>
-                  <option className={`text-center`}>ENG</option>
+                  <option value="CRO">CRO</option>
+                  <option>ENG</option>
                 </select>
                 {/* Toggle for Light/Dark Mode */}
-                <label
-                  id="toggleMode"
-                  className="relative inline-flex items-center cursor-pointer ml-2"
-                >
-                  <DarkModeSwitch
-                    id={"toggleMode"}
-                    checked={!isLightTheme}
-                    onChange={() => {
-                      localStorage.setItem("theme", `"${!isLightTheme}"`);
-                      chooseTheme(!isLightTheme);
-                      setCssTheme(!isLightTheme ? "light" : "dark");
-                    }}
-                    size={30}
-                  />
+                <label htmlFor="toggleMode" className="sr-only">
+                  Toggle Theme
                 </label>
+                <DarkModeSwitch
+                  className="ml-3"
+                  id="toggleMode"
+                  checked={!isLightTheme}
+                  onChange={() => {
+                    localStorage.setItem("theme", `"${!isLightTheme}"`);
+                    chooseTheme(!isLightTheme);
+                    setCssTheme(!isLightTheme ? "light" : "dark");
+                  }}
+                  size={30}
+                />
               </div>
             </div>
           </div>
         </div>
+
+        {/* Mobile Navigation Menu */}
         <div
           className={`fixed z-40 w-full overflow-hidden flex flex-col lg:hidden gap-12 origin-top duration-700 ${
             !toggleMenu ? "h-0" : "h-full"
@@ -211,58 +216,72 @@ const Navbar: FC<IProps> = ({
           <div
             className={`px-8 theme-${className}-main theme-${className}-text`}
           >
-            <div className="flex flex-col gap-8 font-bold">
-              <Link
-                to="/"
-                className="hover:hover:text-red-500"
-                onClick={handleLinkClick}
-              >
-                {" "}
-                {langName === "eng"
-                  ? textContent.eng.first_route
-                  : textContent.cro.first_route}
-              </Link>
-              <Link
-                to="/services"
-                className="hover:hover:text-red-500"
-                onClick={handleLinkClick}
-              >
-                {langName === "eng"
-                  ? textContent.eng.second_route
-                  : textContent.cro.second_route}
-              </Link>
-              <Link
-                to="/gallery"
-                className="hover:hover:text-red-500"
-                onClick={handleLinkClick}
-              >
-                {langName === "eng"
-                  ? textContent.eng.third_route
-                  : textContent.cro.third_route}
-              </Link>
-              <Link
-                to="/about"
-                className="hover:hover:text-red-500"
-                onClick={handleLinkClick}
-              >
-                {langName === "eng"
-                  ? textContent.eng.fourth_route
-                  : textContent.cro.fourth_route}
-              </Link>
-              <Link
-                to="/contact"
-                className="hover:hover:text-red-500"
-                onClick={handleLinkClick}
-              >
-                {langName === "eng"
-                  ? textContent.eng.fifth_route
-                  : textContent.cro.fifth_route}
-              </Link>
-            </div>
+            <ul className="flex flex-col gap-8 font-bold">
+              <li role="none">
+                <Link
+                  to="/"
+                  role="menuitem"
+                  className="hover:text-red-500"
+                  onClick={handleLinkClick}
+                >
+                  {langName === "eng"
+                    ? textContent.eng.first_route
+                    : textContent.cro.first_route}
+                </Link>
+              </li>
+              <li role="none">
+                <Link
+                  to="/services"
+                  role="menuitem"
+                  className="hover:text-red-500"
+                  onClick={handleLinkClick}
+                >
+                  {langName === "eng"
+                    ? textContent.eng.second_route
+                    : textContent.cro.second_route}
+                </Link>
+              </li>
+              <li role="none">
+                <Link
+                  to="/gallery"
+                  role="menuitem"
+                  className="hover:text-red-500"
+                  onClick={handleLinkClick}
+                >
+                  {langName === "eng"
+                    ? textContent.eng.third_route
+                    : textContent.cro.third_route}
+                </Link>
+              </li>
+              <li role="none">
+                <Link
+                  to="/about"
+                  role="menuitem"
+                  className="hover:text-red-500"
+                  onClick={handleLinkClick}
+                >
+                  {langName === "eng"
+                    ? textContent.eng.fourth_route
+                    : textContent.cro.fourth_route}
+                </Link>
+              </li>
+              <li role="none">
+                <Link
+                  to="/contact"
+                  role="menuitem"
+                  className="hover:text-red-500"
+                  onClick={handleLinkClick}
+                >
+                  {langName === "eng"
+                    ? textContent.eng.fifth_route
+                    : textContent.cro.fifth_route}
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
-    </div>
+    </header>
   );
 };
 
